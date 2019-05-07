7484cbe32796142e0fe31afc303bb1d88da13426
Ezekiel Chow <ezekielchow94@gmail.com>
2019-05-07 12:19:56 +0800

    clean up code 187

--- solutions/187.js
+++ solutions/187.js
@@ -1,81 +1,83 @@
-// true
-// let data = [
-//   {
-//     top_left: [1, 4],
-//     dimensions: [3, 3]
-//   },
-//   {
-//     top_left: [-1, 3],
-//     dimensions: [2, 1]
-//   },
-//   {
-//     top_left: [0, 5],
-//     dimensions: [4, 3]
-//   }
-// ];
+let data = [];
 
 // true
-// let data = [
-//   {
-//     top_left: [0, 1],
-//     dimensions: [1, 1]
-//   },
-//   {
-//     top_left: [1, 1],
-//     dimensions: [1, 1]
-//   }
-// ];
+data.push([
+  {
+    top_left: [1, 4],
+    dimensions: [3, 3]
+  },
+  {
+    top_left: [-1, 3],
+    dimensions: [2, 1]
+  },
+  {
+    top_left: [0, 5],
+    dimensions: [4, 3]
+  }
+]);
 
 // true
-// let data = [
-//   {
-//     top_left: [0, 1],
-//     dimensions: [1, 1]
-//   },
-//   {
-//     top_left: [0, 1],
-//     dimensions: [1, 1]
-//   }
-// ];
+data.push([
+  {
+    top_left: [0, 1],
+    dimensions: [1, 1]
+  },
+  {
+    top_left: [1, 1],
+    dimensions: [1, 1]
+  }
+]);
+
+// true
+data.push([
+  {
+    top_left: [0, 1],
+    dimensions: [1, 1]
+  },
+  {
+    top_left: [0, 1],
+    dimensions: [1, 1]
+  }
+]);
 
 // false
-// let data = [
-//   {
-//     top_left: [-5, 10],
-//     dimensions: [1, 1]
-//   },
-//   {
-//     top_left: [-3, 9],
-//     dimensions: [1, 1]
-//   }
-// ];
+data.push([
+  {
+    top_left: [-5, 10],
+    dimensions: [1, 1]
+  },
+  {
+    top_left: [-3, 9],
+    dimensions: [1, 1]
+  }
+]);
 
 // false
-// let data = [
-//   {
-//     top_left: [-2, 5],
-//     dimensions: [1, 1]
-//   },
-//   {
-//     top_left: [-4, 3],
-//     dimensions: [1, 1]
-//   }
-// ];
+data.push([
+  {
+    top_left: [-2, 5],
+    dimensions: [1, 1]
+  },
+  {
+    top_left: [-4, 3],
+    dimensions: [1, 1]
+  }
+]);
 
 // false
-// let data = [
-//   {
-//     top_left: [3, 3],
-//     dimensions: [1, 1]
-//   },
-//   {
-//     top_left: [5, 1],
-//     dimensions: [1, 1]
-//   }
-// ];
+data.push([
+  {
+    top_left: [3, 3],
+    dimensions: [1, 1]
+  },
+  {
+    top_left: [5, 1],
+    dimensions: [1, 1]
+  }
+]);
 
 // false
-let data = [
+data.push([
   {
     top_left: [1, 1],
     dimensions: [1, 1]
@@ -84,53 +86,50 @@ let data = [
     top_left: [4, 4],
     dimensions: [1, 1]
   }
-];
+]);
 
 console.time("benchmark");
 
-data.forEach(rect => {
-  rect.bottom_right = [
-    rect.top_left[0] + rect.dimensions[0],
-    rect.top_left[1] - rect.dimensions[1]
-  ];
+data.forEach(aSet => {
+  aSet.forEach(rect => {
+    console.log(rect);
+    rect.bottom_right = [
+      rect.top_left[0] + rect.dimensions[0],
+      rect.top_left[1] - rect.dimensions[1]
+    ];
+  });
 });
-
-console.log(data);
 
 let rectOverlaps = true;
 
-for (let i = 0; i < data.length; i++) {
-  for (let x = 0; x < data.length; x++) {
-    if (x === i) {
-      continue;
+data.forEach(aSet => {
+  for (let i = 0; i < aSet.length; i++) {
+    for (let x = 0; x < aSet.length; x++) {
+      if (x === i) {
+        continue;
+      }
+
+      const rect_0 = aSet[i];
+      const rect_1 = aSet[x];
+
+      if (
+        // both rectangles are beside each other
+        rect_1.top_left[0] > rect_0.bottom_right[0] ||
+        rect_0.top_left[0] > rect_1.bottom_right[0] ||
+        // both rectangles are above each other
+        rect_0.bottom_right[1] > rect_1.top_left[1] ||
+        rect_1.bottom_right[1] > rect_0.top_left[1]
+      ) {
+        rectOverlaps = false;
+        break;
+      }
     }
-
-    const rect_0 = data[i];
-    const rect_1 = data[x];
-
-    if (
-      // both rectangles are beside each other
-      rect_1.top_left[0] > rect_0.bottom_right[0] ||
-      rect_0.top_left[0] > rect_1.bottom_right[0]
-    ) {
-      rectOverlaps = false;
-      break;
-    }
-
-    if (
-      // both rectangles are beside each other
-      rect_0.bottom_right[1] > rect_1.top_left[1] ||
-      rect_1.bottom_right[1] > rect_0.top_left[1]
-    ) {
-      rectOverlaps = false;
+    if (!rectOverlaps) {
       break;
     }
   }
-  if (!rectOverlaps) {
-    break;
-  }
-}
 
-console.log(`rectangle overlaps: ${rectOverlaps}`);
+  console.log(`rectangle overlaps: ${rectOverlaps}`);
+});
 
 console.timeEnd("benchmark");
